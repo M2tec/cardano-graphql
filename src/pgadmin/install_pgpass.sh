3eabdb0a1b50aa01f07cf46c5f56202efaa06ccd
M2tec <info@m2tec.nl>
2025-03-28 10:58:30 +0100

    Add pgadmin to docker compose

---
+++ src/pgadmin/install_pgpass.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -x
+
+# Set password
+mkdir -p /pgpass
+PGADMIN_PASSWORD_FILE=/pgpass/pgpass
+rm -rf $PGADMIN_PASSWORD_FILE;
+
+cat >$PGADMIN_PASSWORD_FILE <<EOL 
+*:*:*:${POSTGRES_USER}:${POSTGRES_PASSWORD}
+EOL
+
+# PG server file
+PGADMIN_SERVER_JSON_FILE=/pgadmin4/servers.json ;
+rm -rf $PGADMIN_SERVER_JSON_FILE ;
+
+cat >$PGADMIN_SERVER_JSON_FILE <<EOL
+{
+    "Servers": {
+        "1": {
+            "Group": "Servers",
+            "Name": "${NETWORK}",
+            "Host": "postgres",
+            "Port": 5432,
+            "MaintenanceDB": "${POSTGRES_DB}",
+            "Username": "${POSTGRES_USER}",
+            "Favorite": true,
+            "ConnectionParameters": {
+                "sslmode": "prefer",
+                "connect_timeout": 10,
+                "passfile": "/pgpass/pgpass"
+            }
+        }
+    }
+}
+EOL
+
+# Set permissions
+chmod 600 $PGADMIN_PASSWORD_FILE ;
+
+
