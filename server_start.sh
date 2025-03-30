0299e66393028f6113d5d38565cacf85cafcfcdb
M2tec <info@m2tec.nl>
2025-03-30 20:46:57 +0200

    Add some parameters from docker

--- server_start.sh
+++ server_start.sh
@@ -2,6 +2,8 @@
 export LOGGER_MIN_SEVERITY=debug
 export CARDANO_NODE_CONFIG_PATH=../../../config/network/preprod/cardano-node/config.json
 export HASURA_URI="http://localhost:8090"
+export ALLOW_INTROSPECTION=true
+export CACHE_ENABLED=true
 
 cd packages/server/dist/
 
